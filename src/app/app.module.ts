41e9f30cd4a8ba2d97f6a63e998fdeb5d3ad408f
nishurishabh <nishurishabh@gmail.com>
2018-09-22 16:08:02 +0100

    New Modules.ts files added!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app.routing.module';
+import { HttpModule } from '@angular/http';
+import { RecipesModule } from './recipes/recipe.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { CustomDropdownDirective } from './shared/custom-dropdown.directive';
+
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app.routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
-import { HttpModule } from '@angular/http';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth.guard.service';
-import { RecipesModule } from './recipes/recipe.module';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
     HttpModule,
     SharedModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService,
     RecipeService,
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { ShoppingListComponent } from "./shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ],
+   imports: [
+       CommonModule,
+       FormsModule,
+       ReactiveFormsModule
+   ] 
+})
+
+export class ShoppingListModule{}
